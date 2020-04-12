d85831c9b5e5e7f39c742798b28ed3e22bf34417
Rustem Khairutdinov <hayr1998@gmail.com>
2020-04-12 17:06:59 +0300

    Added company controller comments for methods

--- src/main/java/ru/itis/controller/CompanyController.java
+++ src/main/java/ru/itis/controller/CompanyController.java
@@ -14,24 +14,37 @@ import java.util.List;
 @RestController("/companies")
 public class CompanyController {
 
+    /*
+     * Метод возвращает список компаний
+     */
     @GetMapping("/list")
     public List<CompanyDto> getCompanies(){
         //TODO
         return null;
     }
 
+    /*
+     * Метод возвращает список компаний с учетом {query} по имени компании
+     */
     @GetMapping("/list/{query}}")
     public List<CompanyDto> getCompaniesByQuery(@PathVariable("query") String query){
         //TODO
         return null;
     }
 
+    /*
+     * Метод возвращает компанию по {company_id}
+     */
     @GetMapping("/company/{company_id}")
     public CompanyDto getCompany(@PathVariable("company_id") Long id){
         //TODO
         return null;
     }
 
+
+    /*
+     * Метод создаёт запрос на создание компании
+     */
     @PostMapping("/company")
     public void postCompany(@RequestBody CompanyCreationDto company, Authentication authentication){
         //TODO
